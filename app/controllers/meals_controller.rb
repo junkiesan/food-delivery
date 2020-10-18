fb70545630e0cba35355f310514d41e8562d507a
Julien Bouland <bouland.julien@gmail.com>
2020-10-18 23:10:24 +0200

    Refacto meal controller

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,15 +1,16 @@
 require_relative "../models/meal.rb"
+# Require view 
 
 class MealsController
-
+  attr_accessor :mealrepository
   def initialize(mealrepository)
     @mealrepository = mealrepository
   end
 
   def add
     name = gets.chomp
-    price =
-    meal = gets.chomp
+    price = gets.chomp.to_i
+    meal = Meal.new(name: name, price: price)
     @mealrepository.add(meal)
   end
 
