9131fe8eb56c3ab1e88ade6f7caad112393419a8
Julien Bouland <bouland.julien@gmail.com>
2020-10-18 20:05:02 +0200

    Started to implement Meal controller

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,3 +1,17 @@
 class MealsController
-  
+
+  def initialize(mealrepository)
+    @mealrepository = mealrepository
+  end
+
+  def add
+    name = gets.chomp
+    price =
+    meal = gets.chomp
+    @mealrepository.push(meal)
+  end
+
+  def list
+    Meal.all
+  end
 end
---
+++ router.rb
@@ -0,0 +1,5 @@
+class Router
+  def initialize(meals_controller)
+    @meals_controller = meals_controller
+  end
+end
