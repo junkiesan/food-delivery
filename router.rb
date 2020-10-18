b6372b9192b5ab6bd1e50513f7d99498c1f2152d
Julien Bouland <bouland.julien@gmail.com>
2020-10-18 23:06:09 +0200

    Implemented Router

--- router.rb
+++ router.rb
@@ -3,4 +3,40 @@ class Router
     @meals_controller = meals_controller
     @running = true
   end
+
+  def run
+    while @running
+      print_menu
+      choice = gets.chomp.to_i
+      print `clear`
+      route_action(choice)
+    end
+  end
+
+  private
+
+  def print_menu
+    puts '--------------------'
+    puts '------- MENU -------'
+    puts '--------------------'
+    puts '1. Add new meal'
+    puts '2. List all meals'
+    # puts '3. Add new customer'
+    # puts '4. List all customers'
+    puts '8. Exit'
+    print '> '
+  end
+
+  def route_action(choice)
+    case choice
+    when 1 then @meals_controller.add
+    when 2 then @meals_controller.list
+    when 8 then stop!
+    else puts 'Try again looser !'
+    end
+  end
+
+  def stop!
+    @running = false
+  end
 end
