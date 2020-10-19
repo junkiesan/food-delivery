8dd78ee9c4bbed3b6d770402cf52a13943959eea
Julien Bouland <bouland.julien@gmail.com>
2020-10-19 13:04:40 +0200

    Implemented customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,18 @@
-class CustomerController
+require_relative '../views/customer_view.rb'
+require_relative '../models/customer.rb'
+# `Customer` Controller
+class CustomersController
   def initialize(customer_repository)
     @customer_repository = customer_repository
+    @customer_view = CustomerView.new
   end
 
   def add
-    
+    name = @customer_view.ask_for('name')
+    address = @customer_view.ask_for('address')
+    customer = Customer.new(name: name, address: address)
+    @customer_repository.create(customer)
+    display_customers
   end
 
   def list
@@ -14,6 +22,7 @@ class CustomerController
   private
 
   def display_customers
-    @customer_repository.all
+    customers = @customer_repository.all
+    @customer_view.display_list(customers)
   end
 end
