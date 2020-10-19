238a8f8437fab5a457f78f625e2043ae3befb554
Julien Bouland <bouland.julien@gmail.com>
2020-10-19 14:26:52 +0200

    Created and implemented employee model

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,7 +1,8 @@
+# `Meal` model
 class Meal
-attr_accessor :id
-attr_reader :name,
-            :price
+  attr_accessor :id
+  attr_reader :name,
+              :price
 
   def initialize(attributes = {})
     @id = attributes[:id]
