106e80674a9544b97b71e83cc2daa2057066975c
Julien Bouland <bouland.julien@gmail.com>
2020-10-23 16:10:33 +0200

    Meal repo refacto with base

--- app/repositories/meal_repository.rb
+++ app/repositories/meal_repository.rb
@@ -1,47 +1,26 @@
-require 'csv'
+require_relative 'base_repository'
 require_relative '../models/meal'
 # 'Meal' Repository
-class MealRepository
-  def initialize(csv_filepath)
-    @csv_filepath = csv_filepath
-    @meals        = []
-    @next_id      = 1
-    load_csv if File.exist?(@csv_filepath)
-  end
-
-  def all
-    @meals
-  end
-
-  def create(meal)
-    meal.id = @next_id
-    @meals << meal
-    @next_id += 1
-    save_to_csv
-  end
-
-  def find(id)
-    @meals.find { |meal| meal.id == id }
+class MealRepository < BaseRepository
+  def update(index, name, price)
+    element = @elements[index]
+    element.name = name
+    element.price = price
+    save_csv
   end
 
   private
 
-  def save_to_csv
-    CSV.open(@csv_filepath, 'wb') do |csv|
-      csv << %i[id name price]
-      @meals.each do |meal|
-        csv << [meal.id, meal.name, meal.price]
-      end
-    end
+  def build_headers
+    %w[id name price]
   end
-  
-  def load_csv
-    csv_options = { headers: :first_row, header_converters: :symbol }
-    CSV.foreach(@csv_filepath, csv_options) do |row|
-      row[:id]    = row[:id].to_i
-      row[:price] = row[:price].to_i
-      @meals << Meal.new(row)
-    end
-    @next_id = @meals.last.id + 1 unless @meals.empty?
+
+  def build_row(element)
+    [element.id, element.name, element.price]
+  end
+
+  def build_element(row)
+    row[:price] = row[:price].to_i
+    Meal.new(row)
   end
 end
