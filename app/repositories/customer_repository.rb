46be9b1cb0fa2eead55623e0dd618f84473a62ff
Julien Bouland <bouland.julien@gmail.com>
2020-10-23 16:11:15 +0200

    Customer repo refacto with base

--- app/repositories/customer_repository.rb
+++ app/repositories/customer_repository.rb
@@ -1,48 +1,26 @@
-require 'csv'
+
+require_relative "base_repository"
 require_relative "../models/customer"
 
-# `Customer` repository
-class CustomerRepository
-  def initialize(csv_filepath)
-    @csv_filepath = csv_filepath
-    @customers    = []
-    @next_id      = 1
-    load_csv if File.exist?(@csv_filepath)
-  end
-
-  def all
-    @customers
-  end
-
-  def create(customer)
-    customer.id = @next_id
-    @customers << customer
-    @next_id += 1
-    save_to_csv
-  end
-
-  def find(id)
-    @customers.find { |customer| customer.id == id}
+class CustomerRepository < BaseRepository
+  def update(index, name, address)
+    element = @elements[index]
+    element.name = name
+    element.address = address
+    save_csv
   end
 
   private
 
-  def save_to_csv
-    CSV.open(@csv_filepath, 'wb') do |csv|
-      csv << %w[id name address]
-      @customers.each do |customer|
-        csv << [customer.id, customer.name, customer.address]
-      end
-    end
+  def build_headers
+    %w[id name address]
   end
 
-  def load_csv
-    csv_options = { headers: :first_row, header_converters: :symbol }
-    CSV.foreach(@csv_filepath, csv_options) do |row|
-      row[:id] = row[:id].to_i
-      row[:address] = row[:address]
-      @customers << Customer.new(row)
-    end
-    @next_id = @customers.last.id + 1 unless @customers.empty?
+  def build_row(element)
+    [element.id, element.name, element.address]
+  end
+
+  def build_element(row)
+    Customer.new(row)
   end
 end
